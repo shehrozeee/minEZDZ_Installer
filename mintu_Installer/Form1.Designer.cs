c1b8b0b4fd73f80dea1d87e179d7bc557e701b7d
Shehroze Malik <shehroze@innidata.com>
2021-04-05 03:49:21 -0400

    first commit

---
+++ mintu_Installer/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace mintu_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Install minDU";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Uninstall minDU";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Delete minDU.exe and\nremove reg entry";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Install minDU.exe  \r\nand perform reg edit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(408, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Pmsg @ Mini_Ninja on IRC for Help , suggestions and feedback";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(397, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 65);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Help";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(470, 151);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = " Mini_Ninja IRC Easy DCC Utility - Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ mintu_Installer/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+
+namespace mintu_Installer
+{
+    public partial class Form1 : Form
+    {
+        Form2 splash = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+            splash.ShowDialog();
+            
+        }
+        private const string MenuName = "*\\shell\\Send via IRC dcc";
+        private const string Command = "*\\shell\\Send via IRC dcc\\command";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Mini_Ninja"))
+            { }
+            else
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Mini_Ninja");
+                    
+            FileStream mintu = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+"\\Mini_Ninja\\minDU.exe", FileMode.Create);
+            mintu.Write(global::mintu_Installer.Properties.Resources.Mini_Ninja_IRC_DCC_utility, 0, global::mintu_Installer.Properties.Resources.Mini_Ninja_IRC_DCC_utility.Length);
+            mintu.Close();
+            RegistryKey regmenu = null;
+            RegistryKey regcmd = null;
+            try
+            {
+                regmenu = Registry.ClassesRoot.CreateSubKey(MenuName);
+                if (regmenu != null)
+                    regmenu.SetValue("", "Send via IRC dcc !");
+                regcmd = Registry.ClassesRoot.CreateSubKey(Command);
+                if (regcmd != null)
+                    regcmd.SetValue("",Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+"\\Mini_Ninja\\minDU.exe %1");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Its embarasing but an error occoured\nRun the installer in admin mode\nRight-Click on installer exe and select\n'Run as Administrator'\nIf problem persists Contact Mini_Ninja on IRC", "OOooppppssss.Its Embarasing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (regmenu != null)
+                    regmenu.Close();
+                if (regcmd != null)
+                    regcmd.Close();
+                return;
+            }
+            finally
+            {
+                if (regmenu != null)
+                    regmenu.Close();
+                if (regcmd != null)
+                    regcmd.Close();
+            }
+            MessageBox.Show("MinDU Installed succesfully", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Thankyou for installing mintDU (Mini_Ninja IRC Easy DCC Utility)\nRight-Click on wanted file and select 'Send via IRC dcc !'\nDrop a thanks msg @ Mini_Ninja on IRC if this app\nhelps you out", "Thanks", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    RegistryKey reg = Registry.ClassesRoot.OpenSubKey(Command);
+                    if (reg != null)
+                    {
+                        reg.Close();
+                        Registry.ClassesRoot.DeleteSubKey(Command);
+                    }
+                    reg = Registry.ClassesRoot.OpenSubKey(MenuName);
+                    if (reg != null)
+                    {
+                        reg.Close();
+                        Registry.ClassesRoot.DeleteSubKey(MenuName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, ex.ToString());
+                }
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Mini_Ninja\\minDU.exe");
+            }
+            catch 
+            {
+
+            }
+            MessageBox.Show("Mintu Uninstalled succesfully", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("minDU (Mini_Ninja IRC Easy DCC)\nRight-Click on wanted file and select 'Send via IRC dcc !'\nDrop a thanks msg @ Mini_Ninja on IRC if this app\nhelps you out", "What is minDU ?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
